bf64045fc70748fd4e3fcbe73e99ef8d0574c4a3
Luc Berger <luc.richard94@yahoo.com>
2018-08-11 14:51:51 +0100

    Reduce results to just cities & population

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
+const cities = [];
+
 fetch(endpoint).then(response => {
     return response.json();
 }).then(data => {
-    console.log(data);
+    for (element of data) {
+        const city = element.city;
+        const population = element.population;
+        cities.push({city: `${city}`, population: `${population}`});
+    }
 });
